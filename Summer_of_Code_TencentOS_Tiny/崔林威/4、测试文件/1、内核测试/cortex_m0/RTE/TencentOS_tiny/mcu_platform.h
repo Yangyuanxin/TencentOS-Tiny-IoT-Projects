2978c6b337d54242003dd5bd8cdecd470d362b5b
clw <1797878653@qq.com>
2021-09-08 11:04:04 +0800

    TencentOS-tiny Pack

---
+++ Summer_of_Code_TencentOS_Tiny/崔林威/4、测试文件/1、内核测试/cortex_m0/RTE/TencentOS_tiny/mcu_platform.h
@@ -0,0 +1,33 @@
+/**
+  * @brief  在将Tencentos tiny软件包移植到工程时，在此处添加MCU头文件
+  */
+//参考示例
+#include "ARMCM0.h"
+#include "core_cm0.h"
+/*
+//ARM Cortex-M0、0+、3、4、7、23、33，将头文件改为如下形式
+#include "ARMCM0.h"
+#include "core_cm0.h"
+//ARM Cortex-M0+
+#include "ARMCM0plus.h"
+#include "core_cm0plus.h"
+//ARM Cortex-M3
+#include "ARMCM3.h"
+#include "core_cm3.h"
+//ARM Cortex-M4
+#include "ARMCM4.h"
+#include "core_cm4.h"
+//ARM Cortex-M7
+#include "ARMCM7.h"
+#include "core_cm7.h"
+//ARM Cortex-M23
+#include "ARMCM23.h"
+#include "core_cm23.h"
+//ARM Cortex-M33
+#include "ARMCM33.h"
+#include "core_cm33.h"
+//stm32f10
+#include "stm32f10x.h"
+#include "core_cm3.h"
+#include "system_stm32f10x.h"
+*/
---
+++ Summer_of_Code_TencentOS_Tiny/崔林威/4、测试文件/3、单片机测试/ALIENTEK_EXPLORER_STM32F407ZGT6/Template/OBJ/mcu_it.d
@@ -0,0 +1,1 @@
+..\obj\mcu_it.o: RTE\Tencentos_tiny\mcu_it.c
